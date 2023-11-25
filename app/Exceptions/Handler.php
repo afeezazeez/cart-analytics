3be6b05068cf00740ccd4e8fc45b0256210ab09a
chillingloccini <chillingloccini1@gmail.com>
2023-11-25 11:07:29 +0100

    Error Handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,13 +2,38 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
+use Illuminate\Http\Response;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+
 
 class Handler extends ExceptionHandler
 {
     /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
      *
      * @var array<int, string>
      */
@@ -20,11 +45,35 @@ class Handler extends ExceptionHandler
 
     /**
      * Register the exception handling callbacks for the application.
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function register(): void
+    public function render($request, Throwable $e)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        if ($e instanceof ModelNotFoundException || $e instanceof NotFoundHttpException) {
+            $error = 'Route not found';
+            if ($e instanceof ModelNotFoundException){
+                $modelName = class_basename($e->getModel());
+                $error = "$modelName not found";
+            }
+            return errorResponse($error,[],null,[],Response::HTTP_NOT_FOUND);
+        }
+        elseif ($e instanceof ValidationException) {
+            return errorResponse('Failed validation',[],$e->validator->errors(),[],Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        elseif ($e instanceof AuthenticationException) {
+            return errorResponse('Unauthorized',[],null, Response::HTTP_UNAUTHORIZED);
+        }
+
+        elseif ($e instanceof ClientErrorException) {
+            return errorResponse($e->getMessage(),[],null,[]);
+        }
+
+        elseif ($e instanceof ThrottleRequestsException) {
+            return errorResponse('Max attempts exceeded.Retry later.',[],null,[],Response::HTTP_TOO_MANY_REQUESTS);
+        }
+
+        return errorResponse($e->getMessage(),[],null,[],Response::HTTP_INTERNAL_SERVER_ERROR);
+
     }
 }
---
+++ app/helpers.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\MessageBag;
+
+
+if (! function_exists('successResponse')) {
+    /**
+     * Return a standard success json response
+     *
+     */
+    function successResponse(array $data = [], int $code = Response::HTTP_OK) : JsonResponse
+    {
+        return response()->json([
+            'success' => 1,
+            'data'    => $data,
+            'error'   => null,
+            'errors'  => [],
+            'extra'   => []
+        ], $code);
+    }
+}
+
+if (! function_exists('errorResponse')) {
+    /**
+     * Return a standard error json response
+     *
+     */
+    function errorResponse(string $error = null, array $data = [], MessageBag $errors = null, array $trace = [],  int $code = Response::HTTP_BAD_REQUEST) : JsonResponse
+    {
+        return response()->json([
+            'success' => 0,
+            'data'    => $data,
+            'error'   => $error,
+            'errors'  => $errors ?? [],
+            'trace'   => $trace
+        ], $code);
+
+
+    }
+}
+
